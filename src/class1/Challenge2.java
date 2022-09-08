a8890b5820eeaab341a91517e17224a233b5ebaf
Cubimal Chik <shubhachakrala@gmail.com>
2022-09-08 14:21:38 +0530

    challenge2 is done

---
+++ src/class1/Challenge2.java
@@ -0,0 +1,29 @@
+package class1;
+
+public class Challenge2 {
+	
+	public static void main(String[] args) {
+		double myDouble = 20.00d;
+		double mySecondDouble = 80.00d;
+		double newDouble = (myDouble + mySecondDouble)*100.00d;
+		System.out.println(newDouble);
+		newDouble = (double) newDouble % 40;
+		System.out.println(newDouble);
+		boolean isZero = (newDouble == 0)? true: false;
+	/*	if(newDouble ==0) {
+			isZero = true;
+		}else {
+			isZero = false;
+			System.out.println("Got some reminder");
+		}*/
+		
+		
+		System.out.println(isZero);
+		
+		if(!isZero) {
+			System.out.println("Got some remainder");
+		}
+		
+	}
+
+}
---
+++ src/class1/CharBoolean.java
@@ -0,0 +1,19 @@
+package class1;
+
+public class CharBoolean {
+
+	public static void main(String[] args) {
+			char myChar = 'D';
+			char myUnicodeChar = '\u0044';
+			
+			System.out.println(myUnicodeChar);
+			
+			char myUnicodeCR = '\u00a9';
+			System.out.println(myUnicodeCR);
+			
+			
+			boolean myTest = true;
+			boolean myFalse = false;
+	}
+
+}
