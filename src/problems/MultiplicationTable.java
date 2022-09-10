faff7ada66023f7e0236f0f3fc2c4b44233d8c36
Cubimal Chik <shubhachakrala@gmail.com>
2022-09-10 17:12:22 +0530

    This is good programming

---
+++ src/problems/MultiplicationTable.java
@@ -0,0 +1,20 @@
+package problems;
+
+import java.util.Scanner;
+
+public class MultiplicationTable {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number");
+		int input = sc.nextInt();
+		
+		for(int i=1; i<=10; i++) {
+			System.out.println(input +" * "+ i + " = " + (input*i));
+		}
+		
+		
+	}
+
+}
---
+++ src/problems/PrintOneToTen.java
@@ -0,0 +1,17 @@
+package problems;
+
+public class PrintOneToTen {
+
+	public static void main(String[] args) {
+		int j=0;
+		for(int i =1; i<=10; i++) {
+		//	System.out.println(i);
+			j += i;
+		}
+		System.out.println(j);
+
+		
+		//sum of the first 10 natural numbers
+	}
+
+}
