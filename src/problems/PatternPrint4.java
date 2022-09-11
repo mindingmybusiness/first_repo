753b6e575701adc098b252b158ca4802970247ae
Cubimal Chik <shubhachakrala@gmail.com>
2022-09-11 17:14:20 +0530

    This is pattern 4

--- src/problems/PatternPrint4.java
+++ src/problems/PatternPrint4.java
@@ -3,7 +3,7 @@ package problems;
 public class PatternPrint4 {
 	public static void main(String[] args) {
 		for(int i=1;i<=5;i++) {
-			for (int j=i; j<=5;j++) {
+			for (int j=i; j<5;j++) {
 				System.out.print(" ");
 			}
 			for(int h=1;h<=i;h++) {
