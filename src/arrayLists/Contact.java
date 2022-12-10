b71fdc62375282821621f7fe3e5d20e650134048
Cubimal Chik <shubhachakrala@gmail.com>
2022-12-10 16:59:18 +0530

    Added arrayList learning

--- src/arrayLists/Contact.java
+++ src/arrayLists/Contact.java
@@ -4,10 +4,8 @@ public class Contact {
 
 	private String name;
 	private String phoneNumber;
-	
-	
+
 	public Contact(String name, String phoneNumber) {
-		super();
 		this.name = name;
 		this.phoneNumber = phoneNumber;
 	}
@@ -15,23 +13,13 @@ public class Contact {
 	public String getName() {
 		return name;
 	}
-	
-	/*
-	 * public void setName(String name) { this.name = name; }
-	 */
-	
+
 	public String getPhoneNumber() {
 		return phoneNumber;
 	}
-	
-	/*
-	 * public void setPhoneNumber(String phoneNumber) { this.phoneNumber =
-	 * phoneNumber; }
-	 */
-	
+
 	public static Contact createContact(String name, String phoneNumber) {
-		return new Contact(name,phoneNumber);
+		return new Contact(name, phoneNumber);
 	}
-	
-	
+
 }
